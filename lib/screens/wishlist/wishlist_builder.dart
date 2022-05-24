d868bf3364a9f947088ac167355fa545a7819977
athulpp <athulpp10@gmail.com>
2022-05-24 19:50:57 +0530

    wishlist_builder.dart const added code cleaned

--- lib/screens/wishlist/wishlist_builder.dart
+++ lib/screens/wishlist/wishlist_builder.dart
@@ -24,36 +24,34 @@ class WishList extends StatelessWidget {
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              // return Center(child: Image.asset('assests/images/Group.png'));
               return SkeletonItem(
                   child: Column(
                 children: [
                   Row(
                     children: [
-                      SkeletonAvatar(
+                      const SkeletonAvatar(
                         style: SkeletonAvatarStyle(
                             shape: BoxShape.circle, width: 50, height: 50),
                       ),
-                      SizedBox(width: 8),
+                      const SizedBox(width: 8),
                       Expanded(
                         child: SkeletonParagraph(
                           style: SkeletonParagraphStyle(
-                              lines: 3,
-                              spacing: 6,
-                              lineStyle: SkeletonLineStyle(
-                                randomLength: true,
-                                height: 10,
-                                borderRadius: BorderRadius.circular(8),
-                                minLength:
-                                    MediaQuery.of(context).size.width / 6,
-                                maxLength:
-                                    MediaQuery.of(context).size.width / 3,
-                              )),
+                            lines: 3,
+                            spacing: 6,
+                            lineStyle: SkeletonLineStyle(
+                              randomLength: true,
+                              height: 10,
+                              borderRadius: BorderRadius.circular(8),
+                              minLength: MediaQuery.of(context).size.width / 6,
+                              maxLength: MediaQuery.of(context).size.width / 3,
+                            ),
+                          ),
                         ),
                       )
                     ],
                   ),
-                  SizedBox(height: 12),
+                  const SizedBox(height: 12),
                   SkeletonParagraph(
                     style: SkeletonParagraphStyle(
                         lines: 3,
@@ -65,7 +63,7 @@ class WishList extends StatelessWidget {
                           minLength: MediaQuery.of(context).size.width / 2,
                         )),
                   ),
-                  SizedBox(height: 12),
+                  const SizedBox(height: 12),
                   SkeletonAvatar(
                     style: SkeletonAvatarStyle(
                       width: double.infinity,
@@ -73,12 +71,12 @@ class WishList extends StatelessWidget {
                       maxHeight: MediaQuery.of(context).size.height / 3,
                     ),
                   ),
-                  SizedBox(height: 8),
+                  const SizedBox(height: 8),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
-                        children: [
+                        children: const [
                           SkeletonAvatar(
                               style:
                                   SkeletonAvatarStyle(width: 20, height: 20)),
@@ -122,7 +120,7 @@ class WishList extends StatelessWidget {
                       style: GoogleFonts.bungee(
                           fontSize: 22,
                           fontWeight: FontWeight.w100,
-                          color: Color.fromARGB(255, 64, 40, 31)),
+                          color: const Color.fromARGB(255, 64, 40, 31)),
                     )
                   ],
                 )),
@@ -135,7 +133,6 @@ class WishList extends StatelessWidget {
                     snapshot.data!.docs[index];
                 return WishListContent(
                   onPressed: () {
-                    print('working');
                     Get.to(() => DetailScreen(
                         productId: documentSnapshot.id,
                         productName: documentSnapshot['name'],
@@ -156,8 +153,4 @@ class WishList extends StatelessWidget {
           }),
     );
   }
-
-  // Widget positionedRemove() {
-  //   return
-  // }
 }
