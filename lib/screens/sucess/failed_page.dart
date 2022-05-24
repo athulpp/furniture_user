5aebe12c9a213d48bbd228d343364cdb9e8dc2f7
athulpp <athulpp10@gmail.com>
2022-05-24 20:49:02 +0530

    failed_page.dart style changed and maked clean code

--- lib/screens/sucess/failed_page.dart
+++ lib/screens/sucess/failed_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 import 'package:userapp/controller/bottom_bar_control.dart';
 
@@ -13,52 +15,82 @@ class FailedPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Container(
-              height: 400,
-              width: 400,
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(15)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Stack(
-                  children: [
-                    const Align(
-                        alignment: Alignment.topCenter,
-                        child: Text(
-                          'Payment Failed',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )),
-                    const Center(
-                        child: CircleAvatar(
-                      backgroundColor: Colors.red,
-                      radius: 80,
-                      child: Icon(
-                        Icons.error,
-                        size: 140,
-                        color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Container(
+                height: 400,
+                width: 400,
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 253, 191, 191),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Stack(
+                    children: [
+                      Align(
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            'Payment Failed',
+                            style: GoogleFonts.amarante(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 26,
+                                color: Colors.redAccent),
+                          )),
+                      SizedBox(
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                  width: 150,
+                                  height: 150,
+                                  child: Lottie.asset(
+                                      'assests/images/error.json')),
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(15),
+                                  child: Text(
+                                    'Your Payment Failed Try Again!',
+                                    style: GoogleFonts.bilbo(
+                                        fontSize: 34,
+                                        fontWeight: FontWeight.bold,
+                                        color: const Color.fromARGB(
+                                            255, 64, 40, 31)),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    )),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context).pushAndRemoveUntil(
-                                  MaterialPageRoute(
-                                      builder: (ctx) => BottomNavigation(
-                                            currentIndex: 0,
-                                          )),
-                                  (route) => false);
-                            },
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all(Colors.black)),
-                            child: const Text('Continue Shopping')),
-                      ),
-                    )
-                  ],
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (ctx) => BottomNavigation(
+                                              currentIndex: 0,
+                                            )),
+                                    (route) => false);
+                              },
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.black)),
+                              child: Text(
+                                'Continue Shopping',
+                                style: GoogleFonts.alef(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/screens/sucess/sucees_page.dart
+++ lib/screens/sucess/sucees_page.dart
@@ -33,7 +33,6 @@ class SucessScreen extends StatelessWidget {
           const SizedBox(
             height: 5,
           ),
-  
           SizedBox(
             child: Center(
                 child: Column(
@@ -41,32 +40,32 @@ class SucessScreen extends StatelessWidget {
               children: [
                 SizedBox(
                   child: Center(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: Lottie.asset('assests/images/sucess.json')),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Text(
-                            'Your order will be delivered soon.\n Thank you for choosing our app!',
-                            style: GoogleFonts.bilbo(
-                                fontSize: 34,
-                                fontWeight: FontWeight.bold,
-                                color: const Color.fromARGB(255, 64, 40, 31)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: Lottie.asset('assests/images/sucess.json')),
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: Text(
+                              'Your order will be delivered soon.\n Thank you for choosing our app!',
+                              style: GoogleFonts.bilbo(
+                                  fontSize: 34,
+                                  fontWeight: FontWeight.bold,
+                                  color: const Color.fromARGB(255, 64, 40, 31)),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  )),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             )),
           ),
-
           SizedBox(
             width: 200,
             child: CustomButton(
