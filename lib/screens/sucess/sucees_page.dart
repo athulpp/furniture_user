64d695e100a577d6c1b6be6b34e70abae0615152
athulpp <athulpp10@gmail.com>
2022-05-24 20:10:44 +0530

    sucess_page.dart modified

--- lib/screens/sucess/sucees_page.dart
+++ lib/screens/sucess/sucees_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:userapp/constants/material_button.dart';
-import 'package:userapp/model/product.dart';
-import 'package:userapp/screens/Homescreen.dart';
 
 import '../../controller/bottom_bar_control.dart';
 
 class SucessScreen extends StatelessWidget {
-
-  const SucessScreen({Key? key,}) : super(key: key);
+  const SucessScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,35 +17,65 @@ class SucessScreen extends StatelessWidget {
       body: SafeArea(
           child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 30,
           ),
-          Center(child: Text('SUCCESS!')),
-          SizedBox(
+          Center(
+              child: Text(
+            'SUCCESS!',
+            style: GoogleFonts.adventPro(
+                fontSize: 36, fontWeight: FontWeight.bold),
+          )),
+          const SizedBox(
             height: 50,
           ),
           Image.asset('assests/images/Group.png'),
-          SizedBox(
+          const SizedBox(
             height: 5,
           ),
-          Image.asset('assests/images/checkmark-circle-fill.png'),
+  
           SizedBox(
-            height: 20,
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Lottie.asset('assests/images/sucess.json')),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Text(
+                            'Your order will be delivered soon.\n Thank you for choosing our app!',
+                            style: GoogleFonts.bilbo(
+                                fontSize: 34,
+                                fontWeight: FontWeight.bold,
+                                color: const Color.fromARGB(255, 64, 40, 31)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
+              ],
+            )),
           ),
+
           SizedBox(
-            width: 300,
-            child: Text(
-              'Your order will be delivered soon.\n Thank you for choosing our app!',
-              textAlign: TextAlign.center,
-            ),
-          ),
-          CustomButton(
-            buttonColor: Colors.black,
-              text: 'Back To Home ',
-              onPressed: () {
-                // Get.to(() => HomeScreen(,));
-                Get.to(() => BottomNavigation());
-              })
+            width: 200,
+            child: CustomButton(
+                buttonColor: Colors.black,
+                text: 'Back To Home ',
+                onPressed: () {
+                  Get.to(() => BottomNavigation());
+                }),
+          )
         ],
       )),
     );
