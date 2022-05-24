926070626bda8697624aa55be292566da528f25d
athulpp <athulpp10@gmail.com>
2022-05-24 19:29:18 +0530

    main.dart const added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 import 'package:userapp/screens/login/splash_screen.dart';
-
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,23 +19,11 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
-          // brightness: Brightness.dark,
           primaryColor: Colors.black,
-          appBarTheme: AppBarTheme(
+          appBarTheme: const AppBarTheme(
             backgroundColor: Colors.black,
-            // foregroundColor: Colors.black //here you can give the text color
           )),
-      // home: StreamBuilder(
-      //     stream: FirebaseAuth.instance.authStateChanges(),
-      //     builder: (context, snapshot) {
-      //       if (snapshot.hasData) {
-      //         return BottomNavigation();
-      //       } else {
-      //         return Login();
-      //       }
-      //     }),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
